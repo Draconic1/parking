aff10a084db330555898e5f00911772040e5cae9
Angelina <71411417+Draconic1@users.noreply.github.com>
2022-12-21 01:33:15 +0300

    Update cart.jsx

--- frontend/src/components/cart.jsx
+++ frontend/src/components/cart.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import {useEffect, useState} from "react";
+import {useSelector, useDispatch} from "react-redux";
 import axios from "axios";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
@@ -8,134 +8,173 @@ import Form from "react-bootstrap/Form";
 import authHeader from "../services/auth-header";
 
 import {
-  setOrders,
-  setOrderStatuses,
-  setUsers,
-  setParkings,
-  deleteOrder,
-  updateOrder,
+    setOrders,
+    setOrderStatuses,
+    setUsers,
+    setParkings,
+    deleteOrder,
+    updateOrder,
 } from "./reducerSlice";
+import Modal from "react-bootstrap/Modal";
 
 const Component = () => {
-  const apiBase = useSelector((state) => state.toolkit.apiBase);
-  const users = useSelector((state) => state.toolkit.users);
-  const parkings = useSelector((state) => state.toolkit.parkings);
-  const orders = useSelector((state) => state.toolkit.orders);
-  const orderStatuses = useSelector((state) => state.toolkit.orderStatuses);
-  const dispatch = useDispatch();
+    const apiBase = useSelector((state) => state.toolkit.apiBase);
+    const users = useSelector((state) => state.toolkit.users);
+    const parkings = useSelector((state) => state.toolkit.parkings);
+    const orders = useSelector((state) => state.toolkit.orders);
+    const orderStatuses = useSelector((state) => state.toolkit.orderStatuses);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    axios.get(`${apiBase}/orders`, { headers: authHeader() }).then((resp) => {
-      dispatch(setOrders(resp.data));
-    });
+    const [userCard, setUserCard] = useState("");
 
-    axios
-      .get(`${apiBase}/orders/info/statuses`, { headers: authHeader() })
-      .then((resp) => {
-        dispatch(setOrderStatuses(resp.data));
-      });
 
-    axios.get(`${apiBase}/users`, { headers: authHeader() }).then((resp) => {
-      dispatch(setUsers(resp.data));
-    });
-
-    axios.get(`${apiBase}/parkings`, { headers: authHeader() }).then((resp) => {
-      dispatch(setParkings(resp.data));
-    });
-  }, [apiBase, dispatch]);
-
-  const deleteCart = (id) => {
-    axios
-      .delete(`${apiBase}/orders/${id}`, { headers: authHeader() })
-      .then((resp) => {
-        dispatch(deleteOrder(id));
-      });
-  };
-  const payCart = () => {
-    const ordersInCart = orders.filter((x) => x.status === 1);
-
-    for (const oic of ordersInCart) {
-      const id = oic.id;
-      const tmp = { ...oic };
-      tmp.status = 2;
-      axios
-        .put(`${apiBase}/orders/${id}`, tmp, { headers: authHeader() })
-        .then((resp) => {
-          dispatch(updateOrder(tmp));
+    useEffect(() => {
+        axios.get(`${apiBase}/orders`, {headers: authHeader()}).then((resp) => {
+            dispatch(setOrders(resp.data));
         });
-    }
-  };
 
-  return (
-    <div className="mb-5">
-      <h3>Корзина</h3>
+        axios
+            .get(`${apiBase}/orders/info/statuses`, {headers: authHeader()})
+            .then((resp) => {
+                dispatch(setOrderStatuses(resp.data));
+            });
 
-      {orders.length > 0 &&
-        users.length > 0 &&
-        orderStatuses.length > 0 &&
-        parkings.length > 0 && (
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Статус</th>
-                <th>Паркинг</th>
-                <th>Удалить</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.length > 0 &&
-                orders
-                  .filter((x) => x.status === 1)
-                  .map((x) => {
-                    const s = parkings.find((el) => +el.id === x.parking_id);
+        axios.get(`${apiBase}/users`, {headers: authHeader()}).then((resp) => {
+            dispatch(setUsers(resp.data));
+        });
 
-                    return (
-                      <tr key={x.id}>
-                        <td>{x.id}</td>
-                        <td>
-                          {orderStatuses &&
-                            orderStatuses.find((e) => +e.val === +x.status)
-                              ?.name}
-                        </td>
-                        <td>
-                          {users.find((el) => +el.id === x.user_id).name}
-                        </td>
-                        <td>{s.address}</td>
-                        <td>
-                          <Button
-                            variant="danger"
-                            style={{
-                              color: "transparent",
-                              textShadow: "0 0 0 white",
-                            }}
-                            onClick={() => deleteCart(x.id)}
-                          >
-                            &#10006;
-                          </Button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-              {!orders.length && (
-                <tr>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-              )}
-            </tbody>
-          </Table>
-        )}
+        axios.get(`${apiBase}/parkings`, {headers: authHeader()}).then((resp) => {
+            dispatch(setParkings(resp.data));
+        });
+    }, [apiBase, dispatch]);
 
-      <Form.Group className="mb-3">
-        <Button variant="primary" onClick={payCart}>
-          Оплатить заказ
-        </Button>
-      </Form.Group>
-    </div>
-  );
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => {
+        setShow(true);
+    };
+
+    const deleteCart = (id) => {
+        axios
+            .delete(`${apiBase}/orders/${id}`, {headers: authHeader()})
+            .then((resp) => {
+                dispatch(deleteOrder(id));
+            });
+    };
+    const payCart = () => {
+        const ordersInCart = orders.filter((x) => x.status === 1);
+
+        for (const oic of ordersInCart) {
+            const id = oic.id;
+            const tmp = {...oic};
+            tmp.status = 2;
+            tmp.card = userCard;
+            axios
+                .put(`${apiBase}/orders/${id}`, tmp, {headers: authHeader()})
+                .then((resp) => {
+                    dispatch(updateOrder(tmp));
+                    handleClose();
+                });
+        }
+    };
+
+
+    return (
+        <div className="mb-5">
+            <h3>Корзина</h3>
+
+            {orders.length > 0 &&
+                users.length > 0 &&
+                orderStatuses.length > 0 &&
+                parkings.length > 0 && (
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Статус</th>
+                            <th>Паркинг</th>
+                            <th>Удалить</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {orders.length > 0 &&
+                            orders
+                                .filter((x) => x.status === 1)
+                                .map((x) => {
+                                    const s = parkings.find((el) => +el.id === x.parking_id);
+
+                                    return (
+                                        <tr key={x.id}>
+                                            <td>{x.id}</td>
+                                            <td>
+                                                {orderStatuses &&
+                                                    orderStatuses.find((e) => +e.val === +x.status)
+                                                        ?.name}
+                                            </td>
+                                            <td>
+                                                {users.find((el) => +el.id === x.user_id).name}
+                                            </td>
+                                            <td>{s.address}</td>
+                                            <td>
+                                                <Button
+                                                    variant="danger"
+                                                    style={{
+                                                        color: "transparent",
+                                                        textShadow: "0 0 0 white",
+                                                    }}
+                                                    onClick={() => deleteCart(x.id)}
+                                                >
+                                                    &#10006;
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                        {!orders.length && (
+                            <tr>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                        )}
+                        </tbody>
+                    </Table>
+                )}
+
+            <Form.Group className="mb-3">
+                <Button variant="primary" onClick={() => handleShow()}>
+                    Оплатить заказ
+                </Button>
+            </Form.Group>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Ввести номер карты для оплаты</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Label>Номер карты</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="card"
+                        placeholder="Введите номер карты"
+                        value={userCard}
+                        onChange={(e) => setUserCard(e.target.value)}
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={payCart}>
+                        Оплатить
+                    </Button>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Закрыть
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+        </div>
+    );
 };
 
 export default Component;
